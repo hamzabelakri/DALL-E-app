6100e9ebb87da89e70462d658940107baca29a4d
hamzabelakri <hamzabelakri92@gmail.com>
2023-01-28 13:58:55 +0100

    reverse map

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -46,11 +46,12 @@ function Home() {
             Showing Resuls for{" "}
             <span className="text-[#222328]">{searchText}</span>:
           </h2>
+          
         )}
 
         <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
           {posts.length === 0 ? (
-            <div className="flex justify-center items-center">
+            <div className="grid content-start">
               <Loader />
             </div>
           ) : (
@@ -60,6 +61,7 @@ function Home() {
                   item.name.toLowerCase().includes(searchText.toLowerCase()) ||
                   item.prompt.toLowerCase().includes(searchText.toLowerCase())
               )
+              .reverse()
               .map((elt) => <Card key={elt._id} elt={elt} />)
           )}
         </div>
